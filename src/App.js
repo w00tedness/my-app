f8326f6437da32974fa8c5e7bf070a03c0981135
Kevin Hemstreet-Grimmer <kjhg345@gmail.com>
2021-12-20 21:46:51 -0500

    life cycle breakdown

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React, { Component } from 'react'
 // import './App.css'
-import PostList from './components/PostList'
-import PostForm from './components/PostForm'
-import Greet from './components/Greet'
-import ClickCounter from './components/ClickCounter'
-import HoverCounter from './components/HoverCounter'
+// import PostList from './components/PostList'
+// import PostForm from './components/PostForm'
+// import Greet from './components/Greet'
+// import ClickCounter from './components/ClickCounter'
+// import HoverCounter from './components/HoverCounter'
+// import LifeCycleA from './components/LifeCycleA'
 
 class App extends Component {
 	render() {
 		return (
 			<div className="App">
-			<ClickCounter/>
-			<HoverCounter/>
+			{/* <LifeCycleA/> */}
+			{/* <ClickCounter/> */}
+			{/* <HoverCounter/> */}
         {/* <Greet /> */}
 				{/* <PostForm /> */}
 				{/* <PostList /> */}
