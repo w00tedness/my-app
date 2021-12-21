3cf640961f50e0abf97af92117aecdd6ccced0e5
Kevin Hemstreet-Grimmer <kjhg345@gmail.com>
2021-12-20 20:19:38 -0500

    add greet fucntional

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React, { Component } from 'react'
 // import './App.css'
 import PostList from './components/PostList'
 import PostForm from './components/PostForm'
+import Greet from './components/Greet'
 
 class App extends Component {
 	render() {
 		return (
 			<div className="App">
+        <Greet />
 				<PostForm />
 				<PostList />
 			</div>
